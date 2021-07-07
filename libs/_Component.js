6ff42647f0e6940f2002725e1bbb8466288d1ab8
xesam <xesamguo@gmail.com>
2021-07-07 22:29:19 +0800

    0.0.8 fix

--- libs/_Component.js
+++ libs/_Component.js
@@ -1,11 +1,6 @@
 export default function _Component(opts = {}) {
-    if (!opts.externalClasses) {
-        opts.externalClasses = [];
-    }
+    opts.externalClasses = opts.externalClasses || [];
     opts.externalClasses.unshift('ui-class');
-    if (!opts.options) {
-        opts.options = {};
-    }
     opts.options = {
         styleIsolation: 'isolated',
         multipleSlots: true,
